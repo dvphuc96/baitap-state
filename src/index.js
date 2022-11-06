aa2056e5c16db2395e0ec2b65e5f23edda84c175
dvphucsgt <dvphuc.sgt@gmail.com>
2022-11-06 17:53:46 +0700

    create app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TryGlasses from "./State/TryGlasses";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <TryGlasses/>
+  </>
+);
